cbac2e67ebefd828b02d79cb77a5cff7dd97f091
AndrreyG <andreigandrabur6@gmail.com>
2026-03-27 21:28:50 +0200

    Created ad entity + controller

---
+++ Controller/AdController.cs
@@ -0,0 +1,69 @@
+﻿using CarWebsite.Api.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebsite.Api.Controller
+{
+     [Route("api/ad")]
+     [ApiController]
+     public class AdController : ControllerBase
+     {
+          private static List<Ad> _ads = new();
+          private static int _nextId = 1;
+
+          [HttpGet("all")]
+          public IActionResult GetAllAds()
+          {
+               return Ok(_ads);
+          }
+
+          [HttpGet("{id}")]
+          public IActionResult GetAdById(int id)
+          {
+               var ad = _ads.FirstOrDefault(a => a.Id == id);
+               if (ad == null)
+               {
+                    return NotFound(new { Message = $"Ad with ID {id} not found" });
+               }
+
+               return Ok(ad);
+          }
+
+          [HttpPost]
+          public IActionResult CreateAd([FromBody] Ad ad)
+          {
+               ad.Id = _nextId++;
+               _ads.Add(ad);
+
+               return Created($"/api/ads/{ad.Id}", ad);
+          }
+
+          [HttpPut("{id}")]
+          public IActionResult UpdateAd(int id, [FromBody] Ad updatedAd)
+          {
+               var existingAd = _ads.FirstOrDefault(a =>a.Id == id);
+               if (existingAd == null)
+               {
+                    return NotFound(new { Message = $"Ad with ID {id} not found" });
+               }
+
+               existingAd.Name = updatedAd.Name;
+               existingAd.Description = updatedAd.Description;
+
+               return Ok(existingAd);
+          }
+
+          [HttpDelete("{id}")]
+          public IActionResult DeleteAd(int id)
+          {
+               var ad = _ads.FirstOrDefault(a =>a.Id == id);
+               if (ad == null)
+               {
+                    return NotFound(new { Message = $"Ad with ID {id} not found" });
+               }
+               _ads.Remove(ad);
+
+               return NoContent();
+          }
+     }
+}
---
+++ Domain/Ad.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWebsite.Api.Domain
+{
+     public class Ad
+     {
+          public int Id { get; set; }
+          public string Name { get; set; } = string.Empty;
+          public float Price { get; set; }
+          public string Description { get; set; } = string.Empty;
+     }
+}
