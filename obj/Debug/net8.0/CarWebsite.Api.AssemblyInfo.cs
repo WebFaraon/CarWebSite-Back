42b9af349203bb73ffaf6143530b7c3b1b6e11b6
AndrreyG <andreigandrabur6@gmail.com>
2026-03-27 20:12:06 +0200

    Added User domain and UserController api endpoint

--- obj/Debug/net8.0/CarWebsite.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarWebsite.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarWebsite.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38daae93953b40300bbef1c58653066143bf3835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079bdd305f2ecdc4653543a80564053203270731")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarWebsite.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarWebsite.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
